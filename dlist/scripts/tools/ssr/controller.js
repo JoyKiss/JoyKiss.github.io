7491ae3988bca9f9da3dd136bdbb022a65e8d1c8
Administrator <Administrator@Win7-2019YJHBMR>
2019-09-27 19:37:35 +0800

    commit

--- dlist/scripts/tools/ssr/controller.js
+++ dlist/scripts/tools/ssr/controller.js
@@ -5,32 +5,65 @@
                                        function($scope,$rootScope,$location,ssrManageLoader,$http){
 		//画面初始化
 		$scope.itemstree=[];
-		alert("666666")
+//		alert("666666")
+//		function successCallback(data) {
+//		    $.each(data.success, function (i, item) {
+//		        $("body").append('<h1>' + item.title + '</h1>');
+//		    });
+//		}
 //		$.ajax({
-//        type:"GET",
-//        url:"www.baidu.com",
-//        dataType:"json", 
-//        success:function(data){
-//           if(data.success){
-//               $("searchResult").html(data.msg);
-//           }else{
-//               $("#searchResult").html("出现错误：" + data.msg);
-//           }
-//        },
-//        error:function(jqXHR){
-//           alert("发生错误："+ jqXHR.status);
-//        }
+//        url:"http://yingyan.baidu.com/api/v3/entity/list", 
+//type : "GET",
+//async : false,
+//dataType : "jsonp",//数据类型为jsonp  
+//jsonp : "successCallback",//服务端用于接收callback调用的function名的参数
+//jsonpCallback: 'successCallback'
+//
+//
 //});
-		 
-		   $http({
-		        method: "GET",
-		        url: "http://v2ray.qlolp.ml",
-		        headers: {"Content-Type":"application/json;charset=utf-8"},
-		        data: ""
-		    }).success(function(result){
-		    	alert(result);
-		    }).error(function(result){
-		    });
+//
+//		let domain='http://api.douban.com/v2/movie/top250'
+//	        fetch(domain,{
+//	            start:0,
+//	            count:20,
+//	            method:'GET',
+//	            mode:'cors'
+//	        }).then(response=>{
+//	            console.log(response)
+//	            console.log(response.json())
+//	            return response.json()
+//	        }).then(res=>{
+//	            console.log(res)
+//	        }).catch(e=>{
+//	            console.log(e)
+//	        })
+		
+		var script = document.createElement('script');
+	    script.type = 'text/javascript';
+
+	    // 传参并指定回调执行函数为onBack
+	    script.src = 'http://yingyan.baidu.com/api/v3/entity/list?callback=onBack';
+	    document.head.appendChild(script);
+
+	    // 回调执行函数
+	    function onBack(res) {
+	        alert(JSON.stringify(res));
+	    }
+//		 
+//		 $.get("http://yingyan.baidu.com/api/v3/entity/list",
+//				  function(json) {
+//				      alert(json);
+//				  });
+
+//		   $http({
+//		        method: "GET",
+//		        url: "https://github.com/JoyKiss/JoyKiss.github.io/blob/master/dlist/scripts/app.js",
+//		        dataType: 'json',
+//		            crossDomain: true
+//		    }).success(function(result){
+//		    	alert(result);
+//		    }).error(function(result){
+//		    });
 		 
 
 }])
