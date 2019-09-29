7d773b7956c735dd47587494d061458d89d61461
Administrator <Administrator@Win7-2019YJHBMR>
2019-09-29 10:51:57 +0800

    c

--- dlist/scripts/tools/ssr/controller.js
+++ dlist/scripts/tools/ssr/controller.js
@@ -38,33 +38,76 @@
 //	            console.log(e)
 //	        })
 		
-		var script = document.createElement('script');
-	    script.type = 'text/javascript';
-
-	    // 传参并指定回调执行函数为onBack
-	    script.src = 'http://yingyan.baidu.com/api/v3/entity/list?callback=onBack';
-	    document.head.appendChild(script);
-
-	    // 回调执行函数
-	    function onBack(res) {
-	        alert(JSON.stringify(res));
-	    }
+//		var script = document.createElement('script');
+//	    script.type = 'text/javascript';
+//
+//	    // 传参并指定回调执行函数为onBack
+//	    script.src = 'http://yingyan.baidu.com/api/v3/entity/list?callback=onBack';
+//	    document.head.appendChild(script);
+//
+//	    // 回调执行函数
+//	    function onBack(res) {
+//	        alert(JSON.stringify(res));
+//	    }
 //		 
 //		 $.get("http://yingyan.baidu.com/api/v3/entity/list",
 //				  function(json) {
 //				      alert(json);
 //				  });
-
-//		   $http({
+//		  $http({
+//	            method: "GET",
+//	            url: "http://yingyan.baidu.com/api/v3/entity/list",
+//	            contentType: "application/json"
+//	        }).then(function successCallback(response) {
+//	            console.log("success:" + response.data.Name)
+//	        }, function errorCallback(response) {
+//	            console.log("error:" + response.data)
+//	        });
+//		ssrManageLoader.getOffDataSource().success(function (data) {
+//			alert(data)
+//         }).error(function(e){
+//         });
+//		$http.jsonp("https://github.com/JoyKiss/JoyKiss.github.io/blob/master/dlist/scripts/app.js?jsonp=badgeabc&siteid=137bd406");
+//		function badgeabc(data){ console.log(data)}
+//		$http.jsonp({
 //		        method: "GET",
 //		        url: "https://github.com/JoyKiss/JoyKiss.github.io/blob/master/dlist/scripts/app.js",
-//		        dataType: 'json',
+//		        dataType: 'jsonp',
 //		            crossDomain: true
 //		    }).success(function(result){
 //		    	alert(result);
 //		    }).error(function(result){
 //		    });
 		 
-
+//		function loadScript(url, func) {
+//			  var head = document.head || document.getElementByTagName('head')[0];
+//			  var script = document.createElement('script');
+//			  script.src = url;
+//
+//			  script.onload = script.onreadystatechange = function(){
+//			    if(!this.readyState || this.readyState=='loaded' || this.readyState=='complete'){
+//			      func();
+//			      script.onload = script.onreadystatechange = null;
+//			    }
+//			  };
+//			  head.insertBefore(script,head.childNodes[0]);
+//			}
+//			window.baidu = {
+//			  sug: function(data){
+//			    console.log(data);
+//			  }
+//			}
+//			loadScript('http://suggestion.baidu.com/su?wd=w',function(){console.log('loaded')});
+		
+		var xhr =  new XMLHttpRequest();
+		xhr.onreadystatechange = function () {
+		    if(xhr.readyState == 4){
+		        if(xhr.status >= 200 && xhr.status < 304 || xhr.status == 304){
+		            console.log(xhr.responseText);
+		        }
+		    }
+		}
+		xhr.open('get', 'http://www.baidu.com');
+		xhr.send(null);
 }])
 }).call(this);
--- dlist/scripts/tools/ssr/service.js
+++ dlist/scripts/tools/ssr/service.js
@@ -1,9 +1,18 @@
 (function() {
   'use strict';
   angular.module('ssrManage.service', [])
-  .factory('ssrManageLoader',['$route',function($http,$route){
+  .factory('ssrManageLoader',['$http','$route',function($http,$route){
 	  return {
-			  
+	      getOffDataSource: function (pager) {
+              return $http({
+                  method: 'get',
+                  url: 'http://yingyan.baidu.com/api/v3/entity/list',
+                  contentType: 'application/json',
+                  headers: {
+                      'Content-Type': 'application/json;charset=UTF-8'
+                  }
+              });
+          },
 			
 	  }
   }]);
